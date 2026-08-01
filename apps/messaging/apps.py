44fc5d1e59d44f8be27a3ffd7aa6062d485fd9b3
reaper-mods <reapermodsx@gmail.com>
2026-08-01 15:51:33 +0530

    Remove unused ready method from MessagingConfig

--- apps/messaging/apps.py
+++ apps/messaging/apps.py
@@ -5,6 +5,3 @@ class MessagingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.messaging'
     verbose_name = 'Messaging System'
-    
-    def ready(self):
-        import apps.messaging.signals  # noqa
