0fcce859197ee4e0f0cef8d9de2d0beacf585c0f
reaper-mods <reapermodsx@gmail.com>
2026-08-01 15:51:04 +0530

    Update apps.py

--- apps/tournaments/apps.py
+++ apps/tournaments/apps.py
@@ -5,6 +5,3 @@ class TournamentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tournaments'
     verbose_name = 'Tournaments & Events'
-    
-    def ready(self):
-        import apps.tournaments.signals  # noqa
